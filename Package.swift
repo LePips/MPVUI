8a87cb6bbd0b66e403c4dcdcc478f65d802b8988
Ethan Pippin <ethanpippin2343@gmail.com>
2026-09-23 12:42:56 -0400

    Add swift-docc-plugin

--- Package.swift
+++ Package.swift
@@ -21,7 +21,9 @@ let package = Package(
             targets: ["MPVUI"]
         ),
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.1.0"),
+    ],
     targets: [
         .target(
             name: "MPVUI",
